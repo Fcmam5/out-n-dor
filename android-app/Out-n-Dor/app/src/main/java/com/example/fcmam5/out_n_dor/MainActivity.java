3c6a83b128f84a81c3915c2dcf1d464843ffd6b6
Fcmam5 <dr.fcmam5@gmail.com>
2017-04-04 12:36:14 +0100

    removed unused import

--- android-app/Out-n-Dor/app/src/main/java/com/example/fcmam5/out_n_dor/MainActivity.java
+++ android-app/Out-n-Dor/app/src/main/java/com/example/fcmam5/out_n_dor/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
