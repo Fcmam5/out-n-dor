1089ecd4cd95a32e9ed68974e0ef061b5e43f6f7
Nisrine <nislafe@gmail.com>
2017-05-15 21:46:25 +0100

    Update MainActivity.java

--- android-app/Out-n-Dor/app/src/main/java/com/example/fcmam5/out_n_dor/MainActivity.java
+++ android-app/Out-n-Dor/app/src/main/java/com/example/fcmam5/out_n_dor/MainActivity.java
@@ -7,15 +7,38 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+   Button boutonSport= (Button)findViewById(R.id.buttonSport);
+    Button boutonPark= (Button)findViewById(R.id.buttonPark);
+    Button boutonLibrary= (Button)findViewById(R.id.buttonLibrary);
+    Button boutonTourism= (Button)findViewById(R.id.buttonTourism);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_principale);
 
     }
 
-    public void rsel(View view) {
-        Intent intent = new Intent(MainActivity.this, ListPlaces.class);
-        startActivity(intent);
+    protected void clickFunction(View view){
+        Intent intent= new Intent(MainActivity.this, ListPlaces.class);
+        switch(view.getId()){
+            case R.id.buttonSport:
+                intent.putExtra("sport",boutonSport.getText().toString().toLowerCase());
+                break;
+            case R.id.buttonLibrary:
+                intent.putExtra("library",boutonLibrary.getText().toString().toLowerCase());
+                break;
+
+            case R.id.buttonPark:
+                intent.putExtra("park",boutonPark.getText().toString().toLowerCase());
+                break;
+            case R.id.buttonTourism:
+                intent.putExtra("tourism",boutonTourism.getText().toString().toLowerCase());
+                break;
+        }
+
+
     }
+
+   
 }
